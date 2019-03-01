b0a572f47c04c1f543c29de98d00cdbd767592bb
weemlyn <1067718901@qq.com>
2019-03-01 15:11:19 +0800

    Initial commit

---
+++ src/test/java/com/xwm/springbootredisdemo/SpringbootredisdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xwm.springbootredisdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootredisdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
